1b009adb8ee6960a13fe414a15dbdfb6a3d36b16
dmatoschallenge <diogo.matos@challenge.pt>
2015-04-22 16:30:37 +0100

    create new project with services example

---
+++ TemperatureApp/app/src/main/java/android/flag/pt/challenge_it/temperatureapp/MainActivity.java
@@ -0,0 +1,40 @@
+package android.flag.pt.challenge_it.temperatureapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Main activity. Entry point of Application to test Services.
+ *
+ * @author Challenge.IT
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * Way to call a new service. Like the Activities, the Services are also invoked using intents.
+                 */
+                startService(new Intent(getApplicationContext(), TemperatureService.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
